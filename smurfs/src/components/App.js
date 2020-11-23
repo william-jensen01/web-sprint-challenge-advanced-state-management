5b6467ddc3a022f02ee5e3e6e387e8bcedc4cc47
William Jensen <williambjensen01@gmail.com>
2020-11-22 21:23:43 -0700

    MVP

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
+import {connect} from 'react-redux'
+import {getSmurfs} from '../actions'
+import SmurfContainer from './SmurfContainer'
+import NewSmurfForm from './Form'
 import "./App.css";
 class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount(){
+    this.props.getSmurfs()
+  }
   render() {
     return (
       <div className="App">
@@ -8,9 +19,17 @@ class App extends Component {
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <NewSmurfForm />
+        <div className='app-container'>
+        <SmurfContainer />
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return state
+}
+
+export default connect(mapStateToProps, {getSmurfs})(App); 
